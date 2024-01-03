322f07b5946b7d9d853883a0947a25473eccb576
Nikita Usov <nikitariusov@gmail.com>
2024-01-03 22:34:18 +0200

    fix: товары которых нету в прайсах теперь будут "нет в наличии"

--- excel_update/main.py
+++ excel_update/main.py
@@ -9,6 +9,9 @@ def update(price, sheet, max_row):
     yellow_fill = PatternFill(start_color='00FFFF00',  # стиль заливки измнененных ячеек.
                               end_color='00FFFF00',
                               fill_type='solid')
+    red_fill = PatternFill(start_color='00FF0000',  # стиль заливки не измененных ячеек.
+                              end_color='00FF0000',
+                              fill_type='solid')
 
     for row in range(start_row, max_row + 1):
         cell_id = sheet[f'{id_cell}{row}']
@@ -16,19 +19,22 @@ def update(price, sheet, max_row):
         cell_available = sheet[f'{available_cell}{row}']
         product_id = cell_id.value
 
-        set_price(price, product_id, cell_price, yellow_fill)
-        set_available(price, product_id, cell_available, yellow_fill)
+        set_price(cell_id, price, product_id, cell_price, yellow_fill, red_fill)
+        set_available(price, product_id, cell_available, yellow_fill, red_fill)
 
 
-def set_price(price, product_id, cell_price, yellow_fill):
+def set_price(cell_id, price, product_id, cell_price, yellow_fill, red_fill):
     if product_id in price and cell_price.value != price[product_id].price:
         cell_price.value = price[product_id].price
         cell_price.fill = yellow_fill
+    elif product_id not in price:
+        cell_id.fill = red_fill
+        cell_price.fill = red_fill
     else:
         pass
 
 
-def set_available(price, product_id, cell_available, yellow_fill):
+def set_available(price, product_id, cell_available, yellow_fill, red_fill):
     if product_id in price:
         if price[product_id].available == "true":
             available = '+'
@@ -37,3 +43,6 @@ def set_available(price, product_id, cell_available, yellow_fill):
         if cell_available.value != available:
             cell_available.value = available
             cell_available.fill = yellow_fill
+    elif product_id not in price:
+        cell_available.fill = red_fill
+        cell_available.value = '-'
